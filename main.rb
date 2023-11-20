f4c7bff57d2564ff78e7388fcd5876f745c0c4f1
Wilson Chu <wilson1.chu@gmail.com>
2023-11-20 03:24:25 -0500

    implement game functionality

--- main.rb
+++ main.rb
@@ -1,33 +1,39 @@
-=begin
-EXAMPLE PROMPT:
-  Player 1: What does 5 plus 3 equal? (puts)
-  (gets.chomp) > 9
-  Player 1: Seriously? No!
-  P1: 2/3 vs P2: 3/3
-  ----- NEW TURN -----
-  Player 2: What does 2 plus 6 equal?
-  (gets.chomp) > 8
-  Player 2: YES! You are correct.
-  P1: 2/3 vs P2: 3/3
-  ----- NEW TURN -----
-  ... first player to get to 0/3 loses, other player wins ...
-  Player 1 wins with a score of 1/3
-  ----- GAME OVER -----
-=end
-
 require './player'
 require './question'
 
-
 puts "Welcome to the TwO-O-Player Math Game!"
 puts "2 players will start out with 3 lives each, and whoever gets to 0 first loses!"
-puts "--- START! ---"
+puts "\n--- START! ---"
+puts "\n"
 
 player1 = Player.new('Player 1')
 player2 = Player.new('Player 2')
 
-puts "#{player1.name}: #{player1.score}/3 vs. #{player2.name}: #{player2.score}/3"
+current_player = player1
 
-new_question = Question.new
+while player1.score > 0 && player2.score > 0
+  new_question = Question.new
 
-puts "#{player1.name}: #{new_question.question}"
+  puts "#{current_player.name}: #{new_question.question}"
+  answer = gets.chomp.to_i
+
+  if answer == new_question.answer
+   puts "YES! You are correct."
+  else
+    puts "Seriously? No!"
+    current_player.lose_life
+  end
+
+  puts "#{player1.name}: #{player1.score}/3 vs. #{player2.name}: #{player2.score}/3"
+  
+  if player1.score > 0 && player2.score > 0
+    puts "----- NEW TURN -----"
+  end
+
+  # Switch between players each round
+  current_player = current_player == player1 ? player2 : player1
+end
+
+puts "\n#{current_player.name} wins with a score of #{current_player.score}/3!"
+puts "----- GAME OVER -----"
+puts "Good bye!"
